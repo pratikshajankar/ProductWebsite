e98d6038d5bcb69e6275d3237e80a55b5153652e
pratikshajankar <164823486+pratikshajankar@users.noreply.github.com>
2024-11-20 16:00:42 +0530

    initialchange

---
+++ src/app/model/class/Product.ts
@@ -0,0 +1,16 @@
+export class Product{
+    pid:number;
+    productname:string;
+    productcategory:string;
+    price:number;
+    productimg:string;
+
+    constructor(){
+        this.pid=1;
+        this.productname="";
+        this.productcategory="";
+        this.price=0;
+        this.productimg="";
+    }
+
+}
